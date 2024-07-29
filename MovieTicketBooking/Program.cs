3ebff24ce9541deee9db862ee5befcd18c714b49
srija1202 <srijarajendran1@gmail.com>
2024-07-29 21:15:00 +0530

    Swagger update

--- MovieTicketBooking/Program.cs
+++ MovieTicketBooking/Program.cs
@@ -130,12 +130,6 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseSwagger();
 app.UseSwaggerUI();
 
